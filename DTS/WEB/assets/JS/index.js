fe8ae76b8b63666c2b6dd23cc1a7254d756bbf08
DerickFerdinands <derickandrewferdinands@gmail.com>
2023-01-09 17:27:21 +0530

    ContentU

---
+++ DTS/WEB/assets/JS/index.js
@@ -0,0 +1,17 @@
+let imgCount=0;
+
+for (let i=1;i<311;i++) {
+    console.log("Append"+i);
+    if (imgCount == 0) {
+        $('#galSec').append(`  <div class="row col-12">
+                    <div class="col-6"><img alt="" src="assets/Tats/(${i}).jpg"></div>
+                    <div class="col-6"></div>
+                </div>`);
+        imgCount++;
+    } else {
+        $('#galSec').append(`<div className="row col-12">
+            <div className="col-6 "></div>
+            <div className="col-6"><img alt="" src="assets/Tats/(${i}).jpg"></div>
+        </div>>`);
+        imgCount = 0;
+}}
